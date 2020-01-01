ee2f887c79bb6cf33e1b05371a18a68807e5af52
PPPy290351 <55251758+PPPy290351@users.noreply.github.com>
2020-01-02 00:52:00 +0800

    Create poc.py

---
+++ CTF_Collection/How2decompyle/poc.py
@@ -0,0 +1,63 @@
+import string
+
+restrictions = [
+ 'uudcjkllpuqngqwbujnbhobowpx_kdkp_',
+ 'f_negcqevyxmauuhthijbwhpjbvalnhnm',
+ 'dsafqqwxaqtstghrfbxzp_x_xo_kzqxck',
+ 'mdmqs_tfxbwisprcjutkrsogarmijtcls',
+ 'kvpsbdddqcyuzrgdomvnmlaymnlbegnur',
+ 'oykgmfa_cmroybxsgwktlzfitgagwxawu',
+ 'ewxbxogihhmknjcpbymdxqljvsspnvzfv',
+ 'izjwevjzooutelioqrbggatwkqfcuzwin',
+ 'xtbifb_vzsilvyjmyqsxdkrrqwyyiu_vb',
+ 'watartiplxa_ktzn_ouwzndcrfutffyzd',
+ 'rqzhdgfhdnbpmomakleqfpmxetpwpobgj',
+ 'qggdzxprwisr_vkkipgftuvhsizlc_pbz',
+ 'jerzhlnsegcaqzathfpuufwunakdtceqw',
+ 'lbvlyyrugffgrwo_v_zrqvqszchqrrljq',
+ 'aiwuuhzbszvfpidwwkl_wynlujbsbhfox',
+ 'vmhrizxtiegxdxsqcdoiyxkffloudwtxg',
+ 'tffjnabob_jbf_qiszdsemczghnjysmah',
+ 'zrqkppvynlkelnevngwlkhgaputhoagtt',
+ 'nl_oojyafwoqccbedijmigpedkdzglq_f',
+ 'cksy_skctjlyxktuzchvstunyvcvabomc',
+ 'ppcxleeguvhvhengmvac_bykhzqohjuei',
+ '_clmaicjrrzhwd_fescyaejtbyefxyihy',
+ 'hhopvwsmjtpjiffzatyhjrev_dwnsidyo',
+ 'sjevtrmkkk_zjalxrxfovjsbcxjx_pskp',
+ 'gnynwuuqypddbsylparpcczqimimqmvdl',
+ 'bxitcmhnmanwuhvjxnqeoiimlegrmkjra']
+
+capital = [
+ 0, 4, 9, 19, 23, 26]
+flag = []
+for i in range(len(restrictions[0])):
+    arr = []
+    mod = False
+    for r in restrictions:
+        arr.append(r[i])
+    for j in range(26):
+        index = chr(j+97)
+        if index in arr:
+            continue
+        else:
+            # print 'find' + str(chr(j+97))
+            print(str(chr(j+97)), end="")
+            flag.append(str(chr(j+97)))
+            mod = True
+    if mod == False:
+        flag.append('_')
+
+cap_flag = ''
+for f in range(len(flag)):
+    if f in capital:
+        cap_flag += flag[f].upper()
+    else:
+        cap_flag += flag[f]
+
+print('\nYeah, you got it !\nBambooFox{' + cap_flag + '}\n')
+
+                
+
+
+        
